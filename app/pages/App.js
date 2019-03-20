02fd4fd673b7ba9482ac38fb235156789e7d0e31
Higor Alves <goodyearalves@gmail.com>
2019-03-20 00:10:02 -0300

    Button with Gradient added

--- app/pages/App.js
+++ app/pages/App.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-
-const instructions = Platform.select({
-	ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-	android:
-		'Double tap R on your keyboard to reload,\n' +
-		'Shake or press menu button for dev menu',
-});
-
+import { StyleSheet, View, Text } from 'react-native';
+import Button from '../components/atoms/button/button';
 export default class App extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<Text style={styles.welcome}>Welcome to React Native!</Text>
-				<Text style={styles.instructions}>To get started, edit App.js</Text>
-				<Text style={styles.instructions}>{instructions}</Text>
+				<Button text='Login' func={() => console.warn('ola mundo')} />
 			</View>
 		);
 	}
@@ -26,15 +17,5 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 		backgroundColor: '#F5FCFF',
-	},
-	welcome: {
-		fontSize: 20,
-		textAlign: 'center',
-		margin: 10,
-	},
-	instructions: {
-		textAlign: 'center',
-		color: '#333333',
-		marginBottom: 5,
-	},
+	}
 });
